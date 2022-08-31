96485c72f6be4cde5a46b511c13ae21d57c53997
Thomas Tursics <thomas@tursics.de>
2022-08-31 18:23:56 +0200

    remove unused logging (remove sheeting)

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function() {
-    EventSystem.addListener('vote.player', function(param) {
+    /* EventSystem.addListener('vote.player', function(param) {
         var playerName = Business.players[param.player].name;
         console.info(`Player ${playerName} vote: ${param.figure}`);
-    });
+    }); */
 
     UI.init();
 });
